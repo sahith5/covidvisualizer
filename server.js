52524392a1f1111cda95cb103b6262d1b95430ff
sahith <bandasahithreddy@gmail.com>
2021-06-08 18:34:51 +0530

    everything added

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app=express();
+
+
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.json())
+
+const port=process.env.PORT;
+
+
+app.listen(port,function(req,res)
+{
+    console.log("started at http://localhost:3000");
+})
+
+
+app.get("/",function(req,res)
+{
+res.sendFile(__dirname+"/frontend/index.html")
+})
